9a9f408c537a3a83502c0c2d5d72b1224b0adbba
ZahinTasnimNowshin <zahin.tasnim894@gmail.com>
2025-12-04 22:23:35 +0600

    Update phy2.c

--- phy2.c
+++ phy2.c
@@ -6,5 +6,6 @@ int red = 700 ;
 double a = (3 * green )/0.9063; //sin(65) = 0.9063
  double angle = ((2*700)/1754.8);
 double rad_angle= asin(angle) *(180.00/3.141592653589793);
- printf("%lf",rad_angle);
+ printf("%lf degree",rad_angle);
 return 0;}
+
